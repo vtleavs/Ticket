855141989c623a40259705fd615328de8e07e375
Ben Leavitt <vtleavs@gmail.com>
2016-02-19 09:20:41 -0500

    Removed residual imports

--- src/ticket/Application.java
+++ src/ticket/Application.java
@@ -6,19 +6,8 @@
 package ticket;
 
 
-import java.awt.Container;
-import java.awt.Graphics;
-import java.awt.HeadlessException;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
+
 import java.util.Scanner;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 /**
  *
